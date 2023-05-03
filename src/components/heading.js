5d0e9f2cd9d14258eb180318c494f99b39ec7d6f
Anh Chau <anhchau8922@gmail.com>
2023-05-03 20:38:56 +0100

    update

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import '../css/anh.css';
+import Navbar from "./navbar/navigator";
+import Home from "./navbar/home";
+import Contact from "./navbar/contact";
+import Login from "./navbar/login";
+import Setting from "./navbar/setting";
 
 
 export default function Heading() {
@@ -7,7 +13,19 @@ export default function Heading() {
     return (
         <div id="header">
             <h1><span>DEV</span>&lt;NEWS/&gt;</h1>
-            <nav></nav>
+            <nav>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Navbar />} >
+                            <Route index element={<Home />}/>
+                            <Route path="/setting" element={<Setting/>}/>
+                            <Route path="/contact" element={<Contact/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+                
+            </nav>
         </div>
     )
 }
